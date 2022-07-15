d02bfb880972783aee4e0ecd75b4638e30de4446
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-07-15 18:50:25 +0200

    simplify

--- contests/1705/E/main.cc
+++ contests/1705/E/main.cc
@@ -7,12 +7,7 @@ bool one(update_sum::S x) { return x.val == x.len; }
 bool zero(update_sum::S x) { return x.val == 0; }
 
 void Main() {
-  ints(n, q);
   int N = 200000;
-  V<int> a(n);
-  cin >> a;
-  V<int> cnt(N + 1);
-  each(e, a)++ cnt[e];
   update_sum::segtree t(N + 100);
   auto add = [&](int i) {
     int j = t.max_right<one>(i);
@@ -24,15 +19,10 @@ void Main() {
     t.apply(i, j, 1);
     t.set(j, {0, 1});
   };
-  rep(i, N + 1) {
-    int x = cnt[i];
-    int j = i;
-    while (x) {
-      if (x & 1) add(j);
-      x >>= 1;
-      ++j;
-    }
-  }
+  ints(n, q);
+  V<int> a(n);
+  cin >> a;
+  each(e, a) add(e);
   rep(q) {
     ints(k, l);
     --k;
