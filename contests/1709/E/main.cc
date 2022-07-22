b9f5981d6c3a1a71a4d2810628a4145106461376
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-07-22 10:02:42 +0200

    fix

--- contests/1709/E/main.cc
+++ contests/1709/E/main.cc
@@ -3,6 +3,23 @@
 #include "codeforces.h"
 #include "graph.h"
 
+class xor_set {
+ public:
+  void all_xor(int x) { x_ ^= x; }
+  V<int> get() const {
+    V<int> v;
+    each(e, st_) v.eb(e ^ x_);
+    return v;
+  }
+  int count(int v) const { return st_.count(v ^ x_); }
+  void insert(int v) { st_.insert(v ^ x_); }
+  int size() const { return st_.size(); }
+
+ private:
+  set<int> st_;
+  int x_ = 0;
+};
+
 void Main() {
   ints(n);
   V<int> a(n);
@@ -10,23 +27,23 @@ void Main() {
   Graph g(n);
   g.Read();
   int ans = 0;
-  Fix([&](auto rec, int node, int parent) -> set<int> {
-    set<int> st;
+  Fix([&](auto rec, int node, int parent) -> xor_set {
+    xor_set st;
     st.insert(0);
     bool ok = true;
     each(child, g[node]) if (child != parent) {
-      set<int> ST = rec(child, node);
+      xor_set ST = rec(child, node);
       if (sz(st) < sz(ST)) swap(st, ST);
-      each(e, ST) if (st.count(e ^ a[node])) ok = false;
-      each(e, ST) st.insert(e);
+      V<int> v = ST.get();
+      each(e, v) if (st.count(e ^ a[node])) ok = false;
+      each(e, v) st.insert(e);
     }
     if (!ok) {
       ++ans;
       return {};
     }
-    set<int> ret;
-    each(e, st) ret.insert(e ^ a[node]);
-    return ret;
+    st.all_xor(a[node]);
+    return st;
   })(0, -1);
   wt(ans);
 }
