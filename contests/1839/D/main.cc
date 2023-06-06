a40ac3d1bed43991ee733eff821e8b1800c990ac
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-06-07 00:46:02 +0200

    add solutions

--- contests/1839/D/main.cc
+++ contests/1839/D/main.cc
@@ -6,14 +6,27 @@ void Main() {
   ints(n);
   V<int> c(n);
   cin >> c;
-  V<int> lis;
-  each(e, c) {
-    auto it = lower_bound(all(lis), e);
-    if (it == lis.end()) {
-      lis.eb(e);
-    } else {
-      *it = e;
+  {
+    V<int> nc;
+    nc.eb(0);
+    each(e, c) nc.eb(e);
+    nc.eb(n + 1);
+    swap(c, nc);
+    n += 2;
+  }
+  vector dp(n, vector(n + 1, big));
+  dp[0][0] = 0;
+  rep(i, n) rep(j, n) {
+    chmin(dp[i][j + 1], dp[i][j]);
+    rep(k, i + 1, n) {
+      if (c[i] < c[k]) {
+        int skipped = k - i - 1;
+        int nj = j + (skipped > 0);
+        chmin(dp[k][nj], dp[i][j] + skipped);
+      }
     }
   }
-  dbg(n - sz(lis));
+  V<int> ans;
+  rep(i, 1, sz(dp[n - 1]) - 2) ans.eb(dp[n - 1][i]);
+  wt(ans);
 }
